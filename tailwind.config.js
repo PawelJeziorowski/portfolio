732dfe1c5e78114387cc62e7ccb1cc54826bfe83
Pawel Jeziorowski <jeziorowski.pawel.pj@gmail.com>
2024-11-07 20:34:47 +0100

    add section 1 & 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing:{
+        '47rem':'47rem',
+        '54rem': '54rem',
+        '26.625rem': '26.625rem',
+        '39rem': '39rem',
+
+
+      },
+      borderRadius:{
+        '200':'12.5rem',
+
+      }
+    },
   },
   plugins: [],
 }
