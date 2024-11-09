27392c88dbdceabdff36adb8605f2f55ad1e6c55
Pawel Jeziorowski <jeziorowski.pawel.pj@gmail.com>
2024-11-09 20:27:43 +0100

    add media quary for <1280px

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
         '54rem': '54rem',
         '26.625rem': '26.625rem',
         '39rem': '39rem',
+        '45rem': '45rem',
         '648px': '648px',
         '398px': '398px',
         '354px': '354px',
