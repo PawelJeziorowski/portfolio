7204686fe7871246a623ae4b8f0fb4651d521e14
Pawel Jeziorowski <jeziorowski.pawel.pj@gmail.com>
2024-11-10 13:46:49 +0100

    add buttons action

---
+++ src/js/skrypt.js
@@ -0,0 +1,40 @@
+
+const toutch = document.querySelector(".toutch--js");
+const known = document.querySelector(".known--js");
+const view = document.querySelector(".view--js");
+const source = document.querySelector(".source--js");
+const view2 = document.querySelector(".view2--js");
+const source2 = document.querySelector(".source2--js");
+const view3 = document.querySelector(".view3--js");
+const source3 = document.querySelector(".source3--js");
+const mail = document.querySelector(".mail--js");
+const git = document.querySelector(".git--js");
+const thing = document.querySelector(".thing--js");
+const linkedin = document.querySelector(".linkedin--js");
+const facebook = document.querySelector(".facebook--js");
+const instagram = document.querySelector(".instagram--js");
+const twitter = document.querySelector(".twitter--js");
+
+
+
+view.addEventListener('click', () => {
+    alert('Work in progres')
+});
+source.addEventListener('click', () => {
+    alert('Work in progres')
+});
+view2.addEventListener('click', () => {
+    alert('Work in progres')
+});
+source2.addEventListener('click', () => {
+    alert('Work in progres')
+});
+view3.addEventListener('click', () => {
+    alert('Work in progres')
+});
+source3.addEventListener('click', () => {
+    alert('Work in progres')
+});
+thing.addEventListener('click', () => {
+    alert('Error, the specified page does not exist')
+});
