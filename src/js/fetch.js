cf16defe550a3be5964022b3896eba75fad54b54
Paweł Jeziorowski <jeziorowski.pawel.pj@gmail.com>
2024-12-07 13:22:17 +0100

    bug fixes, perfomers and acsessibility

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -11,7 +11,7 @@ fetch(`https://api.github.com/users/${username}/repos`)
         let tags = ``;
 
         for (let tag of topics){
-            tags +=`<p class="bg-slate-400 rounded-sm w-auto p-1">${tag}</p>`
+            tags +=`<p class="bg-slate-600 rounded-sm h-7 px-4 w-auto p-1">${tag}</p>`
         }
 
         const element = `<div class=" w-648px mr-4 overflow-clip bg-gradient-to-br from-white/10 to-white/5 rounded-3xl max-xl:mb-5 max-xl:h-full max-xl:w-72">
@@ -36,11 +36,11 @@ fetch(`https://api.github.com/users/${username}/repos`)
                         </div>
                         <div class="text-gray-400 text-xl mb-4 ">${description}</div>
                         <div aria-label='${tags}'
-                            class="text-white h-7 w-auto flex justify-start items-centerm flex-wrap gap-4 mb-10 max-xl:h-3 max-xl:text-sm">
+                            class="text-white px-4 h-auto w-auto flex justify-start items-center flex-wrap gap-4 mb-10 max-xl:mb-0 max-xl:h-auto max-xl:text-sm">
                                 ${tags}
                         </div>
                         <div class="flex max-xl:flex-col gap-4 max-xl:mt-8">
-                            <button id="view"
+                            <a href="${homepage}" target="_blank" rel="noopener noreferrer" id="view"
                                 class="group w-64 h-16 text-xl text-lime-500 font-bold hover:text-lime-700 group-hover:stroke-lime-700  duration-500 bg-black rounded-2xl flex flex-row gap-1 place-content-center items-center max-xl:w-40 view--js">
                                 <svg class="stroke-lime-500 group-hover:stroke-lime-700 duration-500" width="24" height="24" viewBox="0 0 24 24" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
@@ -48,8 +48,8 @@ fetch(`https://api.github.com/users/${username}/repos`)
                                         d="M4.00002 21.8174C4.6026 22 5.41649 22 6.8 22H17.2C18.5835 22 19.3974 22 20 21.8174M4.00002 21.8174C3.87082 21.7783 3.75133 21.7308 3.63803 21.673C3.07354 21.3854 2.6146 20.9265 2.32698 20.362C2 19.7202 2 18.8802 2 17.2V6.8C2 5.11984 2 4.27976 2.32698 3.63803C2.6146 3.07354 3.07354 2.6146 3.63803 2.32698C4.27976 2 5.11984 2 6.8 2H17.2C18.8802 2 19.7202 2 20.362 2.32698C20.9265 2.6146 21.3854 3.07354 21.673 3.63803C22 4.27976 22 5.11984 22 6.8V17.2C22 18.8802 22 19.7202 21.673 20.362C21.3854 20.9265 20.9265 21.3854 20.362 21.673C20.2487 21.7308 20.1292 21.7783 20 21.8174M4.00002 21.8174C4.00035 21.0081 4.00521 20.5799 4.07686 20.2196C4.39249 18.6329 5.63288 17.3925 7.21964 17.0769C7.60603 17 8.07069 17 9 17H15C15.9293 17 16.394 17 16.7804 17.0769C18.3671 17.3925 19.6075 18.6329 19.9231 20.2196C19.9948 20.5799 19.9996 21.0081 20 21.8174M16 9.5C16 11.7091 14.2091 13.5 12 13.5C9.79086 13.5 8 11.7091 8 9.5C8 7.29086 9.79086 5.5 12 5.5C14.2091 5.5 16 7.29086 16 9.5Z"
                                         stroke="" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                                 </svg>
-                               <a href="${homepage}" target="_blank" rel="noopener noreferrer"> View demo </a> </button>
-                            <button id="source"
+                               <p > View demo </p> </a>
+                            <a href="${html_url}" target="_blank" rel="noopener noreferrer" id="source"
                                 class="group w-64 h-16 text-xl text-lime-500 hover:text-lime-700 group-hover:stroke-lime-700 translate-colors duration-500 font-bold bg-black rounded-2xl flex flex-row gap-1 place-content-center items-center max-xl:w-40 source--js">
                                 <svg class="stroke-lime-500 text-lime-500  group-hover:stroke-lime-700 translate-colors duration-500" width="24" height="24" viewBox="0 0 24 24" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
@@ -57,7 +57,7 @@ fetch(`https://api.github.com/users/${username}/repos`)
                                         d="M4.00002 21.8174C4.6026 22 5.41649 22 6.8 22H17.2C18.5835 22 19.3974 22 20 21.8174M4.00002 21.8174C3.87082 21.7783 3.75133 21.7308 3.63803 21.673C3.07354 21.3854 2.6146 20.9265 2.32698 20.362C2 19.7202 2 18.8802 2 17.2V6.8C2 5.11984 2 4.27976 2.32698 3.63803C2.6146 3.07354 3.07354 2.6146 3.63803 2.32698C4.27976 2 5.11984 2 6.8 2H17.2C18.8802 2 19.7202 2 20.362 2.32698C20.9265 2.6146 21.3854 3.07354 21.673 3.63803C22 4.27976 22 5.11984 22 6.8V17.2C22 18.8802 22 19.7202 21.673 20.362C21.3854 20.9265 20.9265 21.3854 20.362 21.673C20.2487 21.7308 20.1292 21.7783 20 21.8174M4.00002 21.8174C4.00035 21.0081 4.00521 20.5799 4.07686 20.2196C4.39249 18.6329 5.63288 17.3925 7.21964 17.0769C7.60603 17 8.07069 17 9 17H15C15.9293 17 16.394 17 16.7804 17.0769C18.3671 17.3925 19.6075 18.6329 19.9231 20.2196C19.9948 20.5799 19.9996 21.0081 20 21.8174M16 9.5C16 11.7091 14.2091 13.5 12 13.5C9.79086 13.5 8 11.7091 8 9.5C8 7.29086 9.79086 5.5 12 5.5C14.2091 5.5 16 7.29086 16 9.5Z"
                                         stroke="" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                                 </svg>
-                                <a href="${html_url}" target="_blank" rel="noopener noreferrer"> Source code </a></button>
+                                <p > Source code </p></a>
                         </div>
                     </div>
 
